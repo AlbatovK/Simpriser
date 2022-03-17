14fbc6fc10e9b85574e21a688f17354369b79960
AlbatovK <albatovkonstantin@yandex.ru>
2022-03-17 20:01:54 +0600

    :hammer: Procfile Deleted

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -jar target/0.0.1.jar
--- src/main/java/com/albatros/simspriser/BucketListController.java
+++ src/main/java/com/albatros/simspriser/BucketListController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@SuppressWarnings("unused")
 public class BucketListController {
 
     private List<BucketList> myBucketList = new ArrayList();
